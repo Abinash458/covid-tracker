c54ec90159c7229fff90df5a75e12f118547b901
Abinash <amohapatra458@gmail.com>
2020-04-18 13:31:54 +0530

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
+import { Cards, Chart, CountryPicker } from "./components";
+
 export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello World</h1>
+                <h1>This is a Covid-19 Tracker</h1>
+                <Cards/>
+                <Chart/>
+                <CountryPicker/>
             </div>
         );
     }
